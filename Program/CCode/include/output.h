8a40750ae6082a610f7951ded6312d844c3a330c
Brooks MacLachlan <brooksel_14@hotmail.com>
2016-07-22 09:54:11 -0400

    Improved the way strings are passed to modules

--- Program/CCode/include/output.h
+++ Program/CCode/include/output.h
@@ -3,6 +3,6 @@
 #ifndef OUTPUT_H_INCLUDED
 #define OUTPUT_H_INCLUDED
 
-void output(char filename[], double time[], double tempWat[], double tempPCM[], double eWat[], double ePCM[], double eTot[], struct parameters params, int sizeOfResults);
+void output(char* filename, double time[], double tempWat[], double tempPCM[], double eWat[], double ePCM[], double eTot[], struct parameters params, int sizeOfResults);
 
 #endif // OUTPUT_H_INCLUDED
